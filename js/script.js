7c79803ec35feaa01e4979c8d5febf0279a9e228
Alliance Tshindayi <allytshindayi@gmail.com>
2022-12-02 09:11:25 +0100

    les corrections

--- js/script.js
+++ js/script.js
@@ -222,13 +222,8 @@ const choisirMot = () => {
 
     return mots[indexAléatoire];
 };
-/**
- * Returns a random integer between min (inclusive) and max (inclusive).
- * The value is no lower than min (or the next integer greater than min
- * if min isn't an integer) and no greater than max (or the next integer
- * lower than max if max isn't an integer).
- * Using Math.round() will give you a non-uniform distribution!
- */
+
+
 const obtenirMotAleatoir = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);

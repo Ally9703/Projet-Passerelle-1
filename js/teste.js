a84e66439852845968a4dba1ab7c9dcfa0c718d0
alliance tshinadyi <allytshindayi@gamil.com>
2022-12-20 14:33:10 +0100

    la correction de la logique avec Javascript

---
+++ js/teste.js
@@ -0,0 +1,12 @@
+// SECTION DES ELEMENTS
+let images = document.querySelectorAll('img');
+let lifeLeft = document.querySelector('.laVie');
+let fullWordResult = document.querySelector('.resultatFinal');
+let result = document.querySelector('.span');
+let buttons = document.querySelectorAll('.button');
+let win = document.querySelector('.won');
+let lose = document.querySelector('.lost');
+let hiddenWord = document.querySelectorAll('.hiddenWord');
+var life = 11;
+lifeLeft.textContent = life;
+var index = -1; 
